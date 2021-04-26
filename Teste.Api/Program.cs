c1f89aaa79ebcdd2832dc36c6630bcd9c4ec5d85
Kalyane Lordao <kalyane.lordao@lavid.ufpb.br>
2021-04-26 11:56:41 -0300

    Initial commit

---
+++ Teste.Api/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Teste.Api
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
